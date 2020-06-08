fe4bc01d3f075a3c80b65eaf6b5e20e4a79dffc6
huangshengqiang <huangshengqiang@kedacom.com>
2020-06-08 14:45:21 +0800

    1

---
+++ myWeb/src/components/menuBar.vue
@@ -0,0 +1,58 @@
+<template>
+    <ol class="menuBar">
+      <li @click="$router.push(item.id)"
+          :data-id="item.id" :key="item.id"
+          :class="{activity: page === item.id}"
+          v-for="item in menuList">{{item.name}}</li>
+      <div class="bar"></div>
+    </ol>
+</template>
+
+<script>
+export default {
+  name: 'menuBar',
+  props: {
+    value: ''
+  },
+  computed: {
+    page () {
+      this.$nextTick(() => {
+        this.paging(this.$route.path)
+      })
+      return this.$route.path
+    }
+  },
+  data () {
+    return {
+      menuList: [
+        {name: '主页', id: '/mainView/mainPage'},
+        {name: '开发工具', id: '/mainView/devTools'}
+      ]
+    }
+  },
+  methods: {
+    paging (tag) {
+      let dom = this.$el.querySelector(`[data-id="${tag}"]`)
+      let bar = this.$el.querySelector('.bar')
+      bar.style.cssText = `left: ${dom.offsetLeft}px;width: ${dom.offsetWidth}px`
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  $color: red;
+  .menuBar{
+    display: flex;position: relative;user-select: none;
+    >li{
+      margin: .5rem 1rem;cursor: pointer;color: gray;
+      &.activity{
+        color: #313131;
+      }
+    }
+    >.bar{
+      position: absolute;bottom: 0;height: .2rem;background-color: #3a8ee6;
+      left: 0;transition: all .5s;border-radius: 1rem;
+    }
+  }
+</style>
